0e2799dcb6c874e2940f752bb1eef328c994ac1c
FrancoAguerre <51492220+FrancoAguerre@users.noreply.github.com>
2019-07-12 16:36:59 -0300

    Add files via upload
    cambios menores en las funciones, falta resolver el error de tomar mas de una palabra por linea

--- funciones_SOPA.py
+++ funciones_SOPA.py
@@ -1,37 +1,46 @@
 import random
-def insertar_palabra_horizontal (BOX_SIZE,palabra,matriz,g,filas_validas,fuente,colores):   #en filas validas se le debe mandar una lista con las filas que todavia no tienen una palabra insertada
-	pos_maxima=16-len(palabra)   
+def insertar_palabra_horizontal (BOX_SIZE,palabra,matriz,g,filas_validas,fuente,colores,caps):   #en filas validas se le debe mandar una lista con las filas que todavia no tienen una palabra insertada
+	pos_maxima=15-len(palabra)   
 	pos=random.choice(range(0,pos_maxima))
 	fila=random.choice(filas_validas)
 	filas_validas.remove(fila)
+	if caps=="mayuscula":
+		palabra=palabra.upper()
+	else:
+		palabra=palabra.lower()
 	for i in palabra:
 		matriz[fila][pos+1]=i #el mas uno es para evitar corrimientos
 		colores[fila][pos+1]="grey"
 		pos=pos+1
 		g.DrawRectangle((pos * BOX_SIZE + 5, fila * BOX_SIZE + 3), (pos * BOX_SIZE + BOX_SIZE + 5, fila * BOX_SIZE + BOX_SIZE + 3), line_color='black',fill_color="white")
 		location= (pos * BOX_SIZE + 18, fila * BOX_SIZE + 17)
-		g.DrawText(i,location , font=fuente)
+		g.DrawText(i,location,font=fuente) 
 	return filas_validas	
 
 
-def insertar_palabra_vertical (BOX_SIZE,palabra,matriz,g,columnas_validas,fuente,colores):
-	pos_maxima=16-len(palabra)
+def insertar_palabra_vertical (BOX_SIZE,palabra,matriz,g,columnas_validas,fuente,colores,caps):
+	pos_maxima=15-len(palabra)
 	pos=random.choice(range(0,pos_maxima))
 	columna=random.choice(columnas_validas)
 	columnas_validas.remove(columna)
+	if caps=="mayuscula":
+		palabra=palabra.upper()
+	else:
+		palabra=palabra.lower()
 	for i in palabra:
 		matriz[pos+1][columna]=i #el mas uno es para evitar corrimientos
 		colores[pos+1][columna]="grey"
 		pos=pos+1
 		g.DrawRectangle((columna * BOX_SIZE + 5, pos * BOX_SIZE + 3), (columna * BOX_SIZE + BOX_SIZE + 5, pos * BOX_SIZE + BOX_SIZE + 3), line_color='black',fill_color="white")
 		location= (columna * BOX_SIZE + 18, pos * BOX_SIZE + 17)
-		g.DrawText(i ,location , font=fuente)
+		g.DrawText(i,location,font=fuente)    
 	return columnas_validas
 
 
 def procesar_horizontal(matriz_letras,matriz_colores,dic_colores):
 	lista_aux=[]
 	tipo_actual_f=" "
+	color=""
 	for row in range(16):
 		try:
 			palabra=""
@@ -44,8 +53,8 @@ def procesar_horizontal(matriz_letras,matriz_colores,dic_colores):
 						if dic_colores[una_clave]==color_actual:
 							tipo_actual_f=una_clave
 					color=dic_colores[tipo_actual_f]
-					if color==color_actual:
-						palabra=palabra+letra_actual	
+				if color==color_actual:
+					palabra=palabra+letra_actual	
 			if palabra != "":
 				l=[palabra,tipo_actual_f]
 				lista_aux.append(l)
@@ -56,9 +65,11 @@ def procesar_horizontal(matriz_letras,matriz_colores,dic_colores):
 
 
 
+
 def procesar_vertical(matriz_letras,matriz_colores,dic_colores):
 	lista_aux=[]
 	tipo_actual_f=" "
+	color=""
 	for col in range(16):
 		try:
 			palabra=""
@@ -71,8 +82,8 @@ def procesar_vertical(matriz_letras,matriz_colores,dic_colores):
 						if dic_colores[una_clave]==color_actual:
 							tipo_actual_f=una_clave
 					color=dic_colores[tipo_actual_f]
-					if color==color_actual:
-						palabra=palabra+letra_actual	
+				if color==color_actual:
+					palabra=palabra+letra_actual	
 			if palabra != "":
 				l=[palabra,tipo_actual_f]
 				lista_aux.append(l)
