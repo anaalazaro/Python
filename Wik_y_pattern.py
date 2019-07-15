638a676dec1ce857301b0a14c7812650789207dc
FrancoAguerre <51492220+FrancoAguerre@users.noreply.github.com>
2019-07-15 15:57:18 -0300

    Solucion de errores
    Manejo de excepciones en cuanto a las palabras ingresadas. Faltan los reportes

--- Wik_y_pattern.py
+++ Wik_y_pattern.py
@@ -21,38 +21,48 @@ def validacion(teclado, diccionario):
 	la funcion valida 1a palabra ingresada y la agrega a un diccionario de palabras validas """
 	web = Wiktionary(language="es")
 	articulo = web.search(teclado)#PALABRA
-	if articulo is None:
-		teclado=singularize(teclado)
-		cambio=False
-		articulo = web.search(teclado)
-	elif articulo is not None:
-		cambio=True 						#booleano que indica si la palabra cambió de plural a singular
-		s = list(filter(lambda x: x.title=="Español", articulo.sections))  
+	cambio=False
+	try:
+		s = list(filter(lambda x: x.title=="Español", articulo.sections))
 		etimologia = list(filter(lambda x: x.title=="Etimología", s[0].children))
-		definicion=etimologia[0].content #DEFINICION  
-		lista = ["Adjetivo","Verbo","Verbo intransitivo","Forma verbal","Verbo transitivo","Forma adjetiva","Sustantivo","Sustantivo masculino","Sustantivo femenino"]
-		lista_verbos=["Verbo","Verbo intransitivo","Forma verbal","Verbo transitivo"]
-		lista_sustantivos=["Sustantivo","Sustantivo masculino","Sustantivo femenino"]
-		lista_adjetivos=["Adjetivo","Forma Adjetiva"]
-		for tipo in lista:
-			tipo_real = list(filter(lambda x: x.title==tipo, s[0].children))
-			if tipo_real:
-				clasificacion=tipo
-		if clasificacion in lista_adjetivos: 
-			clasificacion="Adjetivo"
-		elif clasificacion in lista_sustantivos:
-		    clasificacion="Sustantivo"
-		elif clasificacion in lista_verbos:
-		    clasificacion="Verbo"
-		validado=True      
-								   
+		if etimologia ==[]:
+			teclado=singularize(teclado)
+			validado=False
+			articulo = web.search(teclado)
+		if articulo is not None:
+			try:
+				cambio=True 						#booleano que indica si la palabra cambió de plural a singular
+				s = list(filter(lambda x: x.title=="Español", articulo.sections))  
+				etimologia = list(filter(lambda x: x.title=="Etimología", s[0].children))
+				definicion=etimologia[0].content #DEFINICION  
+				lista = ["Adjetivo","Verbo","Verbo intransitivo","Forma verbal","Verbo transitivo","Forma adjetiva","Sustantivo","Sustantivo masculino","Sustantivo femenino"]
+				lista_verbos=["Verbo","Verbo intransitivo","Forma verbal","Verbo transitivo"]
+				lista_sustantivos=["Sustantivo","Sustantivo masculino","Sustantivo femenino"]
+				lista_adjetivos=["Adjetivo","Forma Adjetiva"]
+				for tipo in lista:
+					tipo_real = list(filter(lambda x: x.title==tipo, s[0].children))
+					if tipo_real:
+						clasificacion=tipo
+				if clasificacion in lista_adjetivos: 
+					clasificacion="Adjetivo"
+				elif clasificacion in lista_sustantivos:
+					clasificacion="Sustantivo"
+				elif clasificacion in lista_verbos:
+					clasificacion="Verbo"
+				validado=True      
+			except IndexError:		#esto previene el error de que la pagina de wik no tenga etimologia (definicion)
+				validado=False
+				definicion="" # lo declaro para evitar un error posterior de referenciar una variable antes de que tenga un valor
+				clasificacion=""						   
+	except AttributeError:
+		validado=False
+		definicion="" # lo declaro para evitar un error posterior de referenciar una variable antes de que tenga un valor
+		clasificacion=""	
 	if cambio:
 		pluralize(teclado)
 								 
-	#lista_de_palabras.append(teclado)
 	indice=teclado
 	diccionario[indice]={"Definición":definicion,"Tipo":clasificacion}#diccionario con la palabra , su deficinicion y su tipo.
-	#diccionario.update(indice)
 	if validado:
 		datos={"info_palabra":diccionario,"validez":True} #si se ingreso correctamente, se modifica la lista y diccionario
 	else:
